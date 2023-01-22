77f8e91dbab772ed04cdaadc39359b813a04e0d3
RedTengu <muratborokov@gmail.com>
2023-01-22 22:39:18 +0300

    fix: исправить обработчик попапа.

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -2,22 +2,20 @@ export default class Popup {
   constructor(popupSelector) {
     this._popupSelector = popupSelector;
     this._closeBtn = this._popupSelector.querySelector('.popup__btn-close');
+
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   openPopup() {
     this._popupSelector.classList.add('popup_opened');
 
-    document.addEventListener('keydown', (evt) => {
-      this._handleEscClose(evt)
-    })
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   closePopup() {
     this._popupSelector.classList.remove('popup_opened');
 
-    document.removeEventListener('keydown', (evt) => {
-      this._handleEscClose(evt)
-    })
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   _handleEscClose(evt) {
